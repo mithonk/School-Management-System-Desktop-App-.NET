c7fb5be15ed62efaba0e5c1cad9a9f6c568bc484
Mithursan Kalanantharasan <mitho9798@gmail.com>
2024-07-15 11:42:02 +0530

    Login, Dashboard UI Forms Created

---
+++ School-Management-System-Desktop-App-.NET/School-Management-System-Desktop-App-.NET/UI/formDashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace School_Management_System_Desktop_App_.NET.UI
+{
+    public partial class formDashboard : Form
+    {
+        public formDashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
