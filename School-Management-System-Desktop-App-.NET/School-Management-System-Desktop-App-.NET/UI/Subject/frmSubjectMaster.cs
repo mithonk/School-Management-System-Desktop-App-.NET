c2295f4327b6f8432252a301d7f2e55cb50c6a5a
Mithursan Kalanantharasan <mitho9798@gmail.com>
2024-07-15 11:34:21 +0530

    Student, Grade, Subject UI Form Created

---
+++ School-Management-System-Desktop-App-.NET/School-Management-System-Desktop-App-.NET/UI/Subject/frmSubjectMaster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace School_Management_System_Desktop_App_.NET.UI.Subject
+{
+    public partial class frmSubjectMaster : Form
+    {
+        public frmSubjectMaster()
+        {
+            InitializeComponent();
+        }
+    }
+}
